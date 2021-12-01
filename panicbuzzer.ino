dab2b3a381be8c52baa94263e764489159589db6
Er. Sukh Kaur <60407262+Sukh-star@users.noreply.github.com>
2021-11-30 18:56:40 -0800

    Add files via upload

---
+++ panicbuzzer.ino
@@ -0,0 +1,43 @@
+/*
+  DigitalReadSerial
+
+  Reads a digital input on pin 2, prints the result to the Serial Monitor
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/DigitalReadSerial
+*/
+
+// digital pin 2 has a pushbutton attached to it. Give it a name:
+int pushButton = 2;
+int buzzer = 3;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(pushButton, INPUT);
+  pinMode(buzzer, OUTPUT);
+  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int buttonState = digitalRead(pushButton);
+  if(buttonState == HIGH)
+  {
+   digitalWrite(buzzer, LOW);
+   // print out the state of the button:
+  Serial.println(buttonState);
+  delay(500);        // delay in between reads for stability 
+  } 
+  else
+  {
+    digitalWrite(buzzer, HIGH);
+   // print out the state of the button:
+  Serial.println(buttonState);
+  delay(500);        // delay in between reads for stability
+  }
+}
